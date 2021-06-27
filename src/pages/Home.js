be9e4688e1bb45214d585875499d90df9d0c29cd
jd-create <jenniferdemeijer@t-online.de>
2021-06-27 14:57:44 +0200

    add basic login form, basic pages, routing

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <>
+            <h1>Homepagina</h1>
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias
+                    qui quo unde?</p>
+            </section>
+            <section>
+                <p>Als je ingelogd bent, bekijk dan de <Link to="/profile">Profielpagina</Link></p>
+                <p>Je kunt ook <Link to="/signin">inloggen</Link> of jezelf <Link to="/signup">registeren</Link> als je nog geen
+                    account hebt.</p>
+            </section>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+function SignIn() {
+    const { handleSubmit, register } = useForm();
+
+    function onSubmit(data) {
+        console.log(data);
+    }
+
+    return (
+        <>
+            <h1>Inloggen</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email-field">
+                    Emailadres:
+                    <input
+                        type="email"
+                        id="email-field"
+                        name="email"
+                        {...register("email")}
+                    />
+                </label>
+
+                <label htmlFor="password-field">
+                    Wachtwoord:
+                    <input
+                        type="password"
+                        id="password-field"
+                        name="password"
+                        {...register("password")}
+                    />
+                </label>
+                <button
+                    type="submit"
+                    className="form-button"
+                >
+                    Inloggen
+                </button>
+            </form>
+            <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
+        </>
+    );
+}
+
+export default SignIn;
