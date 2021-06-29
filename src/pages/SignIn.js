c8b7acba01bd5aa5e0ea61afe756097e270c378f
jd-create <jenniferdemeijer@t-online.de>
2021-06-29 18:24:47 +0200

    error and loading in signin

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import axios from "axios";
 import { authContext } from "../contexts/AuthContext";
+import { useHistory } from "react-router-dom";
 
 // - [x] Importeer axios
 // - [x] Maak een asynchrone functie
@@ -19,10 +20,15 @@ import { authContext } from "../contexts/AuthContext";
 
 function SignIn() {
     const { login } = useContext(authContext);
+    const [loading, toggleLoading] = useState(false);
     const { handleSubmit, register } = useForm();
     // console.log('WAT ZIT IN AUTHDATA:',login);
+    const history = useHistory();
+    const [error, setError] = useState("")
 
     async function onSubmit(data) {
+        setError('');
+        toggleLoading(true);
         try {
             // console.log("DATA UIT FORMULIER??", data);
             const response = await axios.post("http://localhost:3000/signin", {
@@ -32,10 +38,13 @@ function SignIn() {
             // console.log("TOKEN?",response.data.accessToken);
             //roep login aan met de token, zo kunnen we hem doorgeven naar de context
             login(response.data.accessToken);
+            setTimeout(() => history.push("/profile"), 2000);
         }   catch (e) {
             console.log('Oh no', e );
+            setError(`Het inloggen is mislukt. Probeer het opnieuw (${e.message})`);
 
         }
+        toggleLoading(false);
     }
 
     return (
@@ -66,9 +75,11 @@ function SignIn() {
                 <button
                     type="submit"
                     className="form-button"
+                    disabled={loading}
                 >
-                    Inloggen
+                    {loading ? "Versturen..." : "Inloggen"}
                 </button>
+                {error && <p className="error-message">{error}</p>}
             </form>
             <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
         </>
