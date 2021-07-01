20afda9eddd4f10326d27f0b67f4677d571d6cad
jd-create <jenniferdemeijer@t-online.de>
2021-07-01 08:05:13 +0200

    add useEffect in profile for fetching  private content

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,21 +1,42 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {authContext} from '../contexts/AuthContext'
+
 
 // - [x] Importeeer de useContext functie en AuthContext
 // - [x] Destucture daar het user object uit
 // - [x] Log het user object
 // - [x] Als er data is, geef die dan weer op de pagina (dus inclusief impliciete check!)
 
+// - [x] Om de data straks ergens op te slaan hebben we state nodig, dus maak een een stukje state aan
+// - [x] De data moet worden opgehaald zodra de pagina geladen is, dus hier hebben we useEffect voor nodig:
+//     - [x] Importeer useEffect
+// - [x] Schrijf de useEffect functie en geef de lege dependency array mee
+// - [ ] Om data op te halen hebben we een asynchrone functie nodig, dus:
+// - [ ] Importeer axios
+// - [] Maak een asynchrone functie in de useEffect en roep hem ook direct aan
+// - [ ] Maak een try / catch blok
+// - [ ] Om beschermde data op te halen hebben we de token nodig! Haal 'm uit de local storage
+// - [ ] In de try: maak een GET request naar het beveiligde eindpoint: http://localhost:3000/660/private-content
+//     - [ ] Een GET request krijgt altijd de url en het config object mee (waarin je request headers - de token! - meegeeft)
+// - [ ] Bekijk de response. Als het succesvol was, plaats dan de response in de state
+// - [ ] Geef de data weer op de pagina (inclusief impliciete check!)
+// - Puntjes op de i: error en laad-tijden inplemententeren (maar dit kun je inmiddels zelf!)
+
 function Profile() {
     const authInfo = useContext(authContext);
     console.log("WHAT'S IN PROFILE:",authInfo.authState.user)
+
+    useEffect(() => {
+        console.log("FETCH DATA IN PROFILE")
+    }, [])
+
     return (
         <>
             <h1>Profielpagina</h1>
             <section>
                 <h2>Gegevens</h2>
-                //het vraagteken hieronder houdt een extra check in of de user wel gevuld is (niet null), optional chaining operator
+                {/*{//het vraagteken hieronder houdt een extra check in of de user wel gevuld is (niet null), optional chaining operator}*/}
                 <p><strong>Gebruikersnaam:</strong> {authInfo.authState.user?.username}</p>
                 <p><strong>Email:</strong> {authInfo.authState.user?.email}</p>
             </section>
