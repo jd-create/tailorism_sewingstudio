f57923f146722cd9f4d9281eae3421e213972119
jd-create <jenniferdemeijer@t-online.de>
2021-07-04 14:26:00 +0200

    Set up context again

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,70 +1,72 @@
-// import {createContext, useState, useEffect} from "react";
-// import jwtDecode from "jwt-decode";
-// import {useHistory} from 'react-router-dom'
-// import axios from "axios";
-//
-// export const authContext = createContext({});
-//
-//
-// function AuthContextProvider(props) {
-//     const history = useHistory();
-//     const [authState, setAuthState] = useState({user: null, status: "pending"});
-//
-//     useEffect(() => {
-//         // TODO we proberen automatisch in te loggen wanneer we nog een token hebben
-//         // setTimeout(() => setAuthState({user: null, status: "done"}), 2000);
-//         const token = localStorage.getItem('token');
-//         if(token) {
-//             login(token);
-//         } else {
-//             setAuthState({user: null, status: "done"});
-//             history.push('/')
-//             }
-//         }, []);
-//
-//
-//     async function getUserData(id,token) {
-//         setAuthState({user: null, status: "pending"});
-//         try {
-//             const response = await axios.get(
-//                 `http://localhost:3000/600/users/${id}`,
-//                 {
-//                     headers: {
-//                         Authorization: `Bearer ${token}`,
-//                     },
-//                 }
-//             );
-//
-//             setAuthState({user: response.data, status: "done"});
-//             history.push('/profile');
-//         } catch (e) {}
-//     }
-//
-//     async function login(token) {
-//         localStorage.setItem('token', token);
-//         const dataFromToken = jwtDecode(token);
-//         console.log("WHAT IS IN THIS TOKEN THING: ", dataFromToken.sub);
-//         const userId = dataFromToken.sub;
-//
-//         getUserData(userId, token);
-//     }
-//
-//     function logout() {
-//         localStorage.removeItem('token');
-//         setAuthState({user: null, status: 'done'});
-//         history.push("/");
-//     }
-//
-//
-//     const data = {authState: authState, login: login, logout: logout};
-//
-//     return (<authContext.Provider value={data}>
-//         {/*hier komt de rest van de app*/}
-//         {authState.status === "pending" && <h1>Fetching your data! Hold on</h1>}
-//         {authState.status === "done" && props.children}
-//     </authContext.Provider>
-//     );
-// }
-//
-// export default AuthContextProvider;
+import {createContext, useState, useEffect} from "react";
+import jwtDecode from "jwt-decode";
+import {useHistory} from 'react-router-dom'
+import axios from "axios";
+
+export const authContext = createContext({});
+
+
+function AuthContextProvider(props) {
+    const history = useHistory();
+    const [authState, setAuthState] = useState({user: null, status: "pending"});
+
+    // useEffect(() => {
+    //     // TODO we proberen automatisch in te loggen wanneer we nog een token hebben
+    //     // setTimeout(() => setAuthState({user: null, status: "done"}), 2000);
+    //     const token = localStorage.getItem('token');
+    //     if(token) {
+    //         login(token);
+    //     } else {
+    //         setAuthState({user: null, status: "done"});
+    //         history.push('/')
+    //         }
+    //     }, []);
+
+
+    // async function getUserData(id,token) {
+    //     setAuthState({user: null, status: "pending"});
+    //     try {
+    //         const response = await axios.get(
+    //             `http://localhost:3000/600/users/${id}`,
+    //             {
+    //                 headers: {
+    //                     Authorization: `Bearer ${token}`,
+    //                 },
+    //             }
+    //         );
+    //
+    //         setAuthState({user: response.data, status: "done"});
+    //         history.push('/profile');
+    //     } catch (e) {}
+    // }
+
+    // async function login(token) {
+    //     localStorage.setItem('token', token);
+    //     const dataFromToken = jwtDecode(token);
+    //     console.log("WHAT IS IN THIS TOKEN THING: ", dataFromToken.sub);
+    //     const userId = dataFromToken.sub;
+    //
+    //     getUserData(userId, token);
+    // }
+    //
+    // function logout() {
+    //     localStorage.removeItem('token');
+    //     setAuthState({user: null, status: 'done'});
+    //     history.push("/");
+    // }
+
+//Hieronder het origineel:
+   // const data = {authState: authState, login: login, logout: logout};
+//Hieronder tijdelijke aanpassing om niet alles ineen te gebruiken
+    const data = {authState: authState};
+
+    return (<authContext.Provider value={data}>
+        {/*hier komt de rest van de app*/}
+        {authState.status === "pending" && <h1>Fetching your data! Hold on</h1>}
+        {authState.status === "done" && props.children}
+    </authContext.Provider>
+    );
+}
+
+export default AuthContextProvider;
 
--- src/index.js
+++ src/index.js
@@ -6,26 +6,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthContextProvider from "./contexts/AuthContext";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <App/>
-        </Router>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Router>
+//             <App/>
+//         </Router>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
 
 //Als de ContextProvider is gemaakt dan moet dit onderste blok ipv het bovenste blok
-// ReactDOM.render(
-//   <React.StrictMode>
-//       <Router>
-//           <AuthContextProvider>
-//     <App />
-//           </AuthContextProvider>
-//       </Router>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+      <Router>
+          <AuthContextProvider>
+    <App />
+          </AuthContextProvider>
+      </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
